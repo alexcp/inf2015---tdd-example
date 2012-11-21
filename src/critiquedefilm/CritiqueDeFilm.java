f93fb6b35ffa049c53c6d3d83be5071719124475
Alexandre Croteau-Pothier <alexcp.com@gmail.com>
2012-11-21 10:23:45 -0500

    nouvelle classe de test

--- src/critiquedefilm/CritiqueDeFilm.java
+++ src/critiquedefilm/CritiqueDeFilm.java
@@ -1,8 +1,27 @@
 package critiquedefilm;
 
+import java.util.ArrayList;
+
 public class CritiqueDeFilm {
 
     public static void main(String[] args) {
-       
+        ArrayList<Film> catalogue = new ArrayList<Film>();
+
+        System.out.println("Ajouter Le Parrain au Catalogue");
+        Film leParrain = new Film("Le parrain","Francis Ford Coppola");
+        catalogue.add(leParrain);
+        leParrain.nouvelleCritique("Roger Ebert","Bla bla bla",4);
+
+        System.out.println("Ajouter Fargo au Catalogue");
+        Film fargo = new Film("Fargo","Coens");
+        catalogue.add(fargo);
+        fargo.nouvelleCritique("Alex cp","Bla bla bla",4);
+        fargo.nouvelleCritique("Jonh Doe","Bla bla bla",3);
+        fargo.nouvelleCritique("Jonh Doe","Bla bla bla",3);
+        fargo.nouvelleCritique("Jonh Doe","Bla bla bla",5);
+
+        //La moyenne des score de Fargo
+        System.out.println(fargo.score());
+
     }
 }
---
+++ test/critiquedefilm/CritiqueDeFilmTest.java
@@ -0,0 +1,29 @@
+package critiquedefilm;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author alex
+ */
+public class CritiqueDeFilmTest {
+
+    public CritiqueDeFilmTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Test
+    public void testMain() {
+    }
+}
